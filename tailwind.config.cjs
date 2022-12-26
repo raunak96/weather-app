6831579fc890acd2f167d0b1fcf6f071de96e19b
Raunak Kedia <raunakkedia1@gmail.com>
2022-12-26 13:04:35 +0530

    Header done

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "hsl(200, 100%, 85%)",
+				secondary: "hsl(200, 100%,10%)",
+				secondaryLight: "hsl(200, 100%,20%)",
+			},
+		},
 	},
 	plugins: [],
 };
